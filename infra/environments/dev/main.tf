0649ffc3d27da446dcf5f36c5640d0c55ac274b4
Benburg-git <sundaybenjaminabiodun@gmail.com>
2026-09-07 05:17:59 +0100

    refactor: move environment configuration to variables

--- infra/environments/dev/main.tf
+++ infra/environments/dev/main.tf
@@ -1,27 +1,13 @@
 module "network" {
-
   source = "../../modules/network"
 
   project_name = var.project_name
+  environment  = var.environment
 
-  environment = var.environment
-
-  vpc_cidr = "10.0.0.0/16"
-
-  public_subnet_cidrs = [
-    "10.0.1.0/24",
-    "10.0.2.0/24"
-  ]
-
-  private_subnet_cidrs = [
-    "10.0.11.0/24",
-    "10.0.12.0/24"
-  ]
-
-  availability_zones = [
-    "eu-west-1a",
-    "eu-west-1b"
-  ]
+  vpc_cidr             = var.vpc_cidr
+  public_subnet_cidrs  = var.public_subnet_cidrs
+  private_subnet_cidrs = var.private_subnet_cidrs
+  availability_zones   = var.availability_zones
 }
 
 
--- infra/environments/dev/variables.tf
+++ infra/environments/dev/variables.tf
@@ -16,6 +16,25 @@ variable "project_name" {
   default = "sentinelpay"
 }
 
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+}
+
+variable "availability_zones" {
+  description = "Availability zones for the VPC"
+  type        = list(string)
+}
 variable "environment" {
 
   description = "Environment"
